d6cbe2266727d43f12aa4a52b90d73be51cd714b
Thomas Michaud <thomas.michaud@gmail.com>
2018-03-19 00:33:48 -0400

    Updating

---
+++ go/src/golang-book/chapter8/httpserver.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/http"
+	"io"
+)
+
+
+// Obviously, test this with a web browser by going to http://localhost:9000/hello
+func hello(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set(
+		"Content-Type", 
+		"text/html",
+	)
+	io.WriteString(
+		res,
+		`<DOCTYPE html>
+		<html>
+		  <head>
+		    <title>Hello World</title>
+		  </head>
+		  <body>
+		    Hello World...from Luna.
+		  </body>
+		</html>`, 
+	)
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+	http.ListenAndServe(":9000", nil)
+}
